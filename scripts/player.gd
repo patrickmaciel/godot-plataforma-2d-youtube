cc04c75a7daefc158226dad4c16f3f4aa29b4cec
Patrick Maciel Campos <patrickmaciel.info@gmail.com>
2026-07-04 15:36:26 -0300

    feat: add walking, jumping animation and flip_h

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,8 @@
 extends CharacterBody2D
 
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
-const SPEED = 30.0
+const SPEED = 50.0
 const JUMP_VELOCITY = -300.0
 
 
@@ -21,5 +22,19 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	# simple way
+	if is_on_floor():
+		# moving to right
+		if direction > 0:
+			anim.flip_h = false
+			anim.play("walk")
+		elif direction < 0:
+			anim.flip_h = true
+			anim.play("walk")
+		else:
+			anim.play("idle")
+	else:
+		anim.play("jump")
 
 	move_and_slide()
