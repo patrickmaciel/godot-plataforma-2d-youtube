126c2359a9284e1fc4a3bd47131def3c56f92335
Patrick Maciel Campos <patrickmaciel.info@gmail.com>
2026-07-05 02:15:49 -0300

    feat: implement decoupled camera, physics and level progression
    Extracts the camera into a separate entity and script, allowing for flexible, scene-specific camera control and better decoupling from the player.
    
    Defines and applies 2D physics layers for "terrain", "player", "enemies", and "level_end" to manage collision interactions effectively.
    
    Introduces a 'LevelEnd' entity with collision detection for basic level completion logic.
    
    Updates existing scenes ('Forest', 'Tropic') to utilize the new camera and level end, adjusting player start positions and camera limits as needed. Additionally, adjusts the global viewport width for consistent display across levels.

---
+++ scripts/camera.gd
@@ -0,0 +1,23 @@
+extends Camera2D
+
+# camera has a goal, a target, that is THE PLAYER
+var target: Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	get_target()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	# position = camera position
+	position = target.position
+
+func get_target():
+	# when load I need to get the player from group
+	var nodes = get_tree().get_nodes_in_group("Player")
+	# get_tree().get_first_node_in_group("Player")
+	if nodes.size() == 0:
+		push_error("Player not found")
+		return
+	
+	target = nodes[0]
