13e9d0e7c10758b12a607c773b8d8d399839533b
Patrick Maciel Campos <patrickmaciel.info@gmail.com>
2026-07-05 22:10:29 -0300

    feat: implement player state machine and tune movement speed
    Refactors player movement and animation logic into explicit states (idle, walk, jump)
    with clear transitions. This improves code readability and maintainability.
    
    Additionally, decreases the player's base speed for better control and gameplay balance.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,101 @@
 extends CharacterBody2D
 
+enum PlayerState {
+	idle,
+	walk,
+	jump
+}
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
-const SPEED = 200.0
+const SPEED = 80.0
 const JUMP_VELOCITY = -300.0
 
+var status: PlayerState
 
+func _ready() -> void:
+	go_to_idle_state()
+	
 func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	match status:
+		PlayerState.idle:
+			idle_satate()
+		PlayerState.walk:
+			walk_state()
+		PlayerState.jump:
+			jump_state()
+	
+	move_and_slide()
+
+func idle_satate():
+	move()
+	if velocity.x != 0:
+		go_to_walk_state()
+		return
+	
+	if Input.is_action_just_pressed("up"):
+		go_to_jump_state()
+		return
+
+func walk_state():
+	move()
+	if velocity.x == 0:
+		go_to_idle_state()
+		return
+		
+	if Input.is_action_just_pressed("up"):
+		go_to_jump_state()
+		return		
+
+func jump_state():
+	move()
+	if is_on_floor():
+		if velocity.x == 0:
+			go_to_idle_state()	
+		else:
+			go_to_walk_state()
+	
+func go_to_idle_state():
+	status = PlayerState.idle
+	anim.play("idle")
+
+func go_to_walk_state():
+	status = PlayerState.walk
+	anim.play("walk")
+
+func go_to_jump_state():
+	status = PlayerState.jump
+	anim.play("jump")
+	velocity.y = JUMP_VELOCITY
+
+func move():
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	if direction < 0:
+		anim.flip_h = true
+	elif direction > 0:
+		anim.flip_h = false
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+func temp(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
