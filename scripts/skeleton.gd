8b1e8978f625d0300088889e6e6402b72a84463f
Patrick Maciel Campos <patrickmaciel.info@gmail.com>
2026-07-06 17:49:50 -0300

    feat: Skeleton collision to walk

--- scripts/skeleton.gd
+++ scripts/skeleton.gd
@@ -2,16 +2,19 @@ extends CharacterBody2D
 
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 @onready var hitbox: Area2D = $Hitbox
+@onready var wall_detector: RayCast2D = $WallDetector
+@onready var ground_detector: RayCast2D = $GroundDetector
 
 enum SkeletonState {
 	walk,
 	dead
 }
 
-const SPEED = 300.0
+const SPEED = 30.0
 const JUMP_VELOCITY = -400.0
 
 var status: SkeletonState
+var direction = 1
 
 func _ready() -> void:
 	go_to_walk_state()
@@ -30,7 +33,18 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	
 func walk_state(delta):
-	pass
+	velocity.x = SPEED * direction
+	if wall_detector.is_colliding():
+		# menos com menos da mais
+		# Less times less equals more
+		# 1 * -1 = -1 / -1 * -1 = 1
+		direction *= -1
+		scale.x *= -1
+		
+	if !ground_detector.is_colliding():
+		direction *= -1
+		scale.x *= -1		
+		
 	
 func go_to_walk_state():
 	status = SkeletonState.walk
@@ -43,6 +57,7 @@ func go_to_dead_state():
 	status = SkeletonState.dead
 	anim.play("dead")
 	hitbox.process_mode = Node.PROCESS_MODE_DISABLED
+	velocity = Vector2.ZERO
 
 func take_damage():
 	go_to_dead_state()
